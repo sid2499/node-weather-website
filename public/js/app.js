7f3178ccff7e2e73d1c01c505a45066f664c0ee4
VishvashDwivedi <66071405+VishvashDwivedi@users.noreply.github.com>
2020-10-01 00:43:05 +0530

    removed unused js

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,6 @@ const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
 const messageTwo=document.querySelector('#message-2')
 
-// messageOne.textContent='From JavaScript'
-
-
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     messageOne.textContent='Loading...'
